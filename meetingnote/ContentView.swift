bb779a1063681564ad81f00dca49f699fae04778
woosublee <dntjqdlekd@gmail.com>
2026-02-07 01:46:59 +0900

    Initial Commit

---
+++ meetingnote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  meetingnote
+//
+//  Created by WOOSUB LEE on 2/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ meetingnote/meetingnoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  meetingnoteApp.swift
+//  meetingnote
+//
+//  Created by WOOSUB LEE on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct meetingnoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
